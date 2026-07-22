36f6aecd5d3764ce9acaa12d80bd79b916eaee0c
zahozako4-creator <zahozako4@gmail.com>
2026-07-22 14:01:28 +0100

    Update MessengerClient.js

--- core/Messenger/MessengerClient.js
+++ core/Messenger/MessengerClient.js
@@ -1,1 +1,68 @@
+const LoginManager = require("./LoginManager");
 
+class MessengerClient {
+	constructor(rim) {
+		this.rim = rim;
+
+		this.api = null;
+
+		this.loginManager = new LoginManager(rim);
+
+		this.connected = false;
+
+		this.startedAt = null;
+	}
+
+	async connect() {
+		if (this.connected)
+			return this.api;
+
+		this.api = await this.loginManager.connect();
+
+		this.connected = true;
+
+		this.startedAt = Date.now();
+
+		this.rim.api = this.api;
+
+		return this.api;
+	}
+
+	isConnected() {
+		return this.connected;
+	}
+
+	getAPI() {
+		return this.api;
+	}
+
+	getUptime() {
+		if (!this.startedAt)
+			return 0;
+
+		return Date.now() - this.startedAt;
+	}
+
+	async disconnect() {
+		if (!this.api)
+			return;
+
+		if (typeof this.api.logout === "function") {
+			try {
+				await this.api.logout();
+			}
+			catch {}
+		}
+
+		this.connected = false;
+		this.api = null;
+		this.rim.api = null;
+	}
+
+	async restart() {
+		await this.disconnect();
+		return this.connect();
+	}
+}
+
+module.exports = MessengerClient;
