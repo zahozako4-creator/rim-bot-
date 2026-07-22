6dd82ac8902418ddb0e52928d49cdd5d6f050412
zahozako4-creator <zahozako4@gmail.com>
2026-07-22 14:35:24 +0100

    Update MessengerClient.js

--- core/Messenger/MessengerClient.js
+++ core/Messenger/MessengerClient.js
@@ -1,21 +1,34 @@
 const LoginManager = require("./LoginManager");
+const Logger = require("../Logger");
 
 class MessengerClient {
+
 	constructor(rim) {
+
 		this.rim = rim;
 
 		this.api = null;
 
-		this.loginManager = new LoginManager(rim);
-
 		this.connected = false;
 
 		this.startedAt = null;
+
+		this.loginManager = new LoginManager(rim);
+
+	}
+
+	async start() {
+
+		return this.connect();
+
 	}
 
 	async connect() {
+
 		if (this.connected)
 			return this.api;
+
+		Logger.system("الاتصال بفيسبوك...");
 
 		this.api = await this.loginManager.connect();
 
@@ -25,44 +38,74 @@ class MessengerClient {
 
 		this.rim.api = this.api;
 
+		Logger.success("MESSENGER", "تم الاتصال بنجاح");
+
 		return this.api;
+
+	}
+
+	async disconnect() {
+
+		if (!this.connected)
+			return;
+
+		try {
+
+			if (typeof this.api.logout === "function")
+				await this.api.logout();
+
+		}
+		catch (err) {
+
+			Logger.warn(
+				"MESSENGER",
+				err.message
+			);
+
+		}
+
+		this.connected = false;
+
+		this.api = null;
+
+		this.rim.api = null;
+
+		Logger.warn(
+			"MESSENGER",
+			"تم قطع الاتصال"
+		);
+
+	}
+
+	async restart() {
+
+		await this.disconnect();
+
+		return this.connect();
+
 	}
 
 	isConnected() {
+
 		return this.connected;
+
 	}
 
 	getAPI() {
+
 		return this.api;
+
 	}
 
 	getUptime() {
+
 		if (!this.startedAt)
 			return 0;
 
 		return Date.now() - this.startedAt;
+
 	}
 
-	async disconnect() {
-		if (!this.api)
-			return;
-
-		if (typeof this.api.logout === "function") {
-			try {
-				await this.api.logout();
-			}
-			catch {}
-		}
-
-		this.connected = false;
-		this.api = null;
-		this.rim.api = null;
-	}
-
-	async restart() {
-		await this.disconnect();
-		return this.connect();
-	}
 }
 
 module.exports = MessengerClient;
