418ce8e3d1df716c91d0129971077a0d77573740
zahozako4-creator <zahozako4@gmail.com>
2026-07-22 14:25:11 +0100

    Update ping.js

--- scripts/system/ping.js
+++ scripts/system/ping.js
@@ -1,1 +1,37 @@
+const Command = require("../../core/structures/Command");
 
+class Ping extends Command {
+
+	constructor() {
+		super({
+			name: "بينغ",
+			aliases: ["ping", "p"],
+			category: "system",
+			description: "عرض سرعة البوت",
+			usage: ".بينغ",
+			cooldown: 3,
+			permissions: 0
+		});
+	}
+
+	async execute(ctx) {
+
+		const start = Date.now();
+
+		await Rim.sender.text(
+			ctx.threadID,
+			"🏓 جاري قياس سرعة البوت..."
+		);
+
+		const ping = Date.now() - start;
+
+		await Rim.sender.text(
+			ctx.threadID,
+			`🏓 سرعة البوت: ${ping} ms`
+		);
+
+	}
+
+}
+
+module.exports = new Ping();
