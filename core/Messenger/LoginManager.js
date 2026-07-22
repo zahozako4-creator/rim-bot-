9c1b21c4a37bf7cd03782ca780557fe8b42e8e8e
zahozako4-creator <zahozako4@gmail.com>
2026-07-22 14:36:48 +0100

    Update LoginManager.js

--- core/Messenger/LoginManager.js
+++ core/Messenger/LoginManager.js
@@ -3,54 +3,86 @@ const path = require("path");
 
 const login = require("fca-unofficial");
 
+const Logger = require("../Logger");
+
 class LoginManager {
 
 	constructor(rim) {
+
 		this.rim = rim;
 
 		this.api = null;
+
+		this.statePath = path.join(
+			process.cwd(),
+			"accounts",
+			"fbstate.json"
+		);
+
 	}
 
-	getAppState() {
+	loadAppState() {
 
-		const statePath = path.join(process.cwd(), "accounts", "fbstate.json");
-
-		if (!fs.existsSync(statePath))
-			throw new Error("لم يتم العثور على fbstate.json");
+		if (!fs.existsSync(this.statePath))
+			throw new Error("لم يتم العثور على accounts/fbstate.json");
 
 		return JSON.parse(
-			fs.readFileSync(statePath, "utf8")
+			fs.readFileSync(this.statePath, "utf8")
 		);
+
+	}
+
+	saveAppState(appState) {
+
+		try {
+
+			fs.writeFileSync(
+				this.statePath,
+				JSON.stringify(appState, null, 2)
+			);
+
+		}
+		catch (err) {
+
+			Logger.warn(
+				"LOGIN",
+				"تعذر حفظ AppState"
+			);
+
+		}
+
 	}
 
 	async connect() {
 
-		const appState = this.getAppState();
+		Logger.system("تسجيل الدخول إلى Facebook...");
+
+		const appState = this.loadAppState();
 
 		return new Promise((resolve, reject) => {
 
 			login(
 				{
-					appState
+					appState,
+
+					listenEvents: true,
+
+					selfListen: false,
+
+					forceLogin: true,
+
+					autoMarkRead: false,
+
+					autoMarkDelivery: false
 				},
+
 				(err, api) => {
 
-					if (err)
-						return reject(err);
+					if (err) {
 
-					this.api = api;
+						Logger.error(
+							"LOGIN",
+							err.stack || err.message
+						);
 
-					this.rim.api = api;
-
-					resolve(api);
-
-				}
-			);
-
-		});
-
-	}
-
-}
-
-module.exports = LoginManager;
+						return reject(err
