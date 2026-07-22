f2b1d72b45ad6dbe20633b6d3551b7736a1c2087
zahozako4-creator <zahozako4@gmail.com>
2026-07-22 14:39:03 +0100

    Update Router.js

--- core/Messenger/Router.js
+++ core/Messenger/Router.js
@@ -1,39 +1,59 @@
 const Parser = require("./Parser");
+const Logger = require("../Logger");
 
 class Router {
+
 	constructor(rim) {
+
 		this.rim = rim;
+
 		this.parser = new Parser(rim);
+
 	}
 
 	async handle(event) {
 
 		const ctx = this.parser.parse(event);
 
-		// ليست أمر
+		// الرسالة ليست أمرًا
 		if (!ctx.isCommand)
 			return;
 
-		const command = this.rim.commands.get(ctx.command);
+		// دعم الـ Alias
+		const commandName =
+			this.rim.loader?.resolveAlias(ctx.command) || ctx.command;
 
-		if (!command)
+		const command = this.rim.commands.get(commandName);
+
+		if (!command) {
+
+			Logger.warn(
+				"ROUTER",
+				`الأمر غير موجود: ${ctx.command}`
+			);
+
 			return;
 
+		}
+
 		try {
+
+			ctx.command = command;
 
 			await command.execute(ctx);
 
 		}
 		catch (err) {
 
-			console.error(
-				`[Router] خطأ داخل الأمر ${ctx.command}\n`,
-				err
+			Logger.error(
+				"ROUTER",
+				err.stack || err.message
 			);
 
 		}
 
 	}
+
 }
 
 module.exports = Router;
